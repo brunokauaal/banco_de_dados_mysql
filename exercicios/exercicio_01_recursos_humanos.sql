724edd5b592b6bd50477c8137a810e32b857ed58
Bruno Kauã <brunokauaalvesdecarvalho@gmail.com>
2023-04-06 13:53:16 -0300

    Exercio 1 sobre rh banco de dados

---
+++ exercicios/exercicio_01_recursos_humanos.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE db_recursoshumanos;
+
+USE db_recursoshumanos;
+
+CREATE TABLE tb_colaboradores(
+id BIGINT AUTO_INCREMENT,
+nome VARCHAR (255) NOT NULL,
+idade INT,
+cargo VARCHAR (255),
+salario DECIMAL NOT NULL,
+primary key(id)
+);
+
+SELECT * FROM tb_colaboradores;
+
+INSERT INTO tb_colaboradores (nome,idade,cargo,salario)
+values ("Bruno",18,"Web Junior",3.000);
+INSERT INTO tb_colaboradores (nome,idade,cargo,salario)
+values ("Rafael",30,"Senior",23.000);
+INSERT INTO tb_colaboradores (nome,idade,cargo,salario)
+values ("Lisa",25,"Senior",23.000);
+INSERT INTO tb_colaboradores (nome,idade,cargo,salario)
+values ("João",30,"Web junior",3.500);
+INSERT INTO tb_colaboradores (nome,idade,cargo,salario)
+values ("Guilherme",23,"Web junior",3.500);
+
+ALTER  TABLE tb_colaboradores MODIFY  salario decimal (6,3);
+SELECT * FROM tb_colaboradores WHERE salario > 2.000;
+
+SELECT * FROM tb_colaboradores WHERE salario < 2.000;
+
+UPDATE tb_colaboradores SET nome = 'Alex' WHERE id = 6;
+UPDATE tb_colaboradores SET salario = 3.500 WHERE id = 1 ;
